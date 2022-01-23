da96084020195823bec8c159bc2d903c828124b0
Ida Dimic <ida@winter.ax>
2022-01-23 10:39:47 +0100

    Initial Commit

---
+++ Stretch+Paws/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stretch+Paws
+//
+//  Created by Ida Dimic on 2022-01-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stretch+Paws/Stretch_PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stretch_PawsApp.swift
+//  Stretch+Paws
+//
+//  Created by Ida Dimic on 2022-01-23.
+//
+
+import SwiftUI
+
+@main
+struct Stretch_PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
